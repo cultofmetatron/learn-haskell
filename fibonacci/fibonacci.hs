fb8e4e4fc16f4381456f65e0f42ea58e1cfbfc93
Peter de Croos <cultofmetatron@aumlogic.com>
2014-08-14 19:19:58 -0700

    added fibbonacci function

---
+++ fibonacci/fibonacci.hs
@@ -0,0 +1,15 @@
+fibonacci 1 = 1
+fibonacci 2 = 1
+fibonacci x = fibonacci(x - 1) + fibonacci(x - 2)
+
+
+fibonacci_iter n counter acc1 acc2 = if (counter == n) 
+  then acc1 + acc2 
+  else fibonacci_iter n (counter + 1) (acc1 + acc2) acc1
+
+fibby 1 = 1
+fibby 2 = 2
+fibby x = fibonacci_iter x 3 1 1
+
+
+
