8799e7126ff138b10a208b2ce3fca53e1f1e8610
Peter de Croos <cultofmetatron@aumlogic.com>
2014-08-31 15:39:00 -0700

    lawl

--- basics/basics.hs
+++ basics/basics.hs
@@ -1,3 +1,21 @@
 doubleMe x = x + x
 
 doubleUs x y = doubleMe x + doubleMe y
+
+mult :: (Num a) => a -> a -> a -> a
+mult x y z = x * y * z
+
+divideByTen :: (Floating a) => a -> a  
+divideByTen = (/10)
+
+divBy :: (Floating a) => a -> a -> a
+divBy a = (/a)
+
+myMap :: (a -> a) -> [a] -> [a]
+myMap f [] = []
+myMap f [a] = [(f a)]
+myMap f (y:ys) = [(f y)] ++ (myMap f ys)
+
+
+
+
--- mergesort/mergesort.hs
+++ mergesort/mergesort.hs
@@ -17,27 +17,11 @@ split [x]       = ([x],[])
 split []        = ([],[])
 
 
---mergeSort :: (Ord a) => [a] -> [a]
+mergeSort :: (Ord a) => [a] -> [a]
 mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort zs = let (ls, rs) = split zs
                in  merge (mergeSort ls) (mergeSort rs) []
 
 
-
-
---slice a -> a -> [b] -> [b]
-
-
-
---mergeSort :: (Ord a) => [a] -> [a]
-
-
-
---[(x, y) | x <- ["rock", "paper", "scizzors"], y <- ["rock", "paper", "scizzors"]]
-
---mergesort [] = []
---mergesort [a, b]
---  | a > b = [b, a]
---  | a < b = [a, b]
 
