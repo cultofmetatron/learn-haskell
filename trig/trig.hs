146116fb2634616448e7f97798d4b24412685afd
Peter de Croos <cultofmetatron@aumlogic.com>
2014-09-01 18:22:30 -0700

    [wip] trig

--- trig/trig.hs
+++ trig/trig.hs
@@ -8,7 +8,7 @@ data Complex a = Complex a a deriving (Show)
 complexPlus :: (Num t) => Complex t -> Complex t -> Complex t
 (Complex r1 i1) `complexPlus` (Complex r2 i2) = Complex (r1 + r2) (i1 + i2)
 
---taylorPolySine :: (Fractional b, Integral b) => b -> b -> b
+taylorPolySine ::  Double -> Double -> Double
 taylorPolySine theta n =  (((-1) ^^ n) / (factorial ((2 * n) + 1))) * (theta ^^ (2 * n + 1))
 
 
